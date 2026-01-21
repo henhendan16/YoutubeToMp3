46ffb9ab697290d8867d741e6e9de92d94fab9b3
henhendan16 <henhendan1.66@gmail.com>
2026-01-21 13:17:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,12 @@ def convert():
     
     # Settings for yt-dlp to extract just the audio
     ydl_opts = {
-        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
         'format': 'bestaudio/best',
         'outtmpl': 'downloaded_audio.%(ext)s',
         'cookiefile': 'cookies.txt',  # <--- ADD THIS LINE
+
+        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+        
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
